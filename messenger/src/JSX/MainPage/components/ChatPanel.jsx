005548f2f16f498e845fe32be66c3e827dd66863
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-07-14 22:47:00 +0700

    Начата работа над ChatPanel

--- messenger/src/JSX/MainPage/components/ChatPanel.jsx
+++ messenger/src/JSX/MainPage/components/ChatPanel.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import cl from '../styles/ChatPanel.module.css'
+import baseavatar from '../../../assets/images/baseavatar.jpg'
 
 const ChatPanel = () => {
-    
+    return (
+        <div className={cl.container}>
+            <div className={cl.profilebox}>
+                <img className={cl.imgbox} src={baseavatar} alt="Profile_Image"/>
+                <div>
+                    <p className={cl.p_username}>Гжегош Бженчишчи</p>
+                    <p className={cl.p_status}>В сети</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ChatPanel;
--- messenger/src/JSX/MainPage/components/MainPage.jsx
+++ messenger/src/JSX/MainPage/components/MainPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import ChatPanel from './ChatPanel.jsx';
 
 const MainPage = () => {
-
+    return (
+        <ChatPanel/>
+    );
 }
-
 export default MainPage;
