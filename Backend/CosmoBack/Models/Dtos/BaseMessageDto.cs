e811d197b3c115a91741b426d91821fe7ff464c2
ip316s26 <nicksh.04@mail.ru>
2025-07-21 19:32:40 +0700

    обобщение структуры DTO для сообщений для быстрого и простого расширения

---
+++ Backend/CosmoBack/Models/Dtos/BaseMessageDto.cs
@@ -0,0 +1,21 @@
+namespace CosmoBack.Models.Dtos
+{
+    public class BaseMessageDto
+    {
+        public Guid Id { get; set; }
+        public Guid SenderId { get; set; }
+        public string Comment { get; set; } = default!;
+        public DateTime CreatedAt { get; set; }
+        public string Username { get; set; } = default!;
+        public Guid? AvatarImageId { get; set; }
+        public MessageReplyDto? ReplyTo { get; set; } 
+    }
+
+    public class MessageReplyDto
+    {
+        public Guid MessageId { get; set; }
+        public Guid SenderId { get; set; }
+        public string Username { get; set; } = default!;
+        public string? Comment { get; set; }
+    }
+}
